986ad172b832171111688b456646508c45c77dea
USER <deeptansu23@gmail.com>
2023-06-23 11:38:28 +0530

    Initial commit

---
+++ src/main/java/pd/workshop/springjpadatademo/SpringJpaDataDemoApplication.java
@@ -0,0 +1,86 @@
+package pd.workshop.springjpadatademo;
+
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pd.workshop.springjpadatademo.model.*;
+import pd.workshop.springjpadatademo.repository.PersonIdCardRepository;
+import pd.workshop.springjpadatademo.repository.PersonRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@SpringBootApplication
+public class SpringJpaDataDemoApplication {
+	static PersonRepository personRepository;
+	static PersonIdCardRepository personIdCardRepository;
+
+	static Faker faker = new Faker();
+
+	public SpringJpaDataDemoApplication(
+			PersonRepository personRepository,
+			PersonIdCardRepository personIdCardRepository){
+		this.personRepository = personRepository;
+		this.personIdCardRepository = personIdCardRepository;
+
+	}
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringJpaDataDemoApplication.class, args);
+
+		Person p1 = new Person(
+				"Akansha",
+				"Patel",
+				"akanshapatel@gmail.com",
+				18
+		);
+		// OneToOne
+		p1.setPersonIdCard(new PersonIdCard("12234",p1));
+		// OneToMany
+		p1.setBooks( List.of(new
+				Book("Clean Code",
+				LocalDateTime.now().minusDays(4),
+				p1)));
+		// Embeddable class - Composite Key
+		p1.addEnrollment(new Enrollment(
+				new EnrollmentId(1L, 1L),
+				p1,
+				new Course("Computer Science", "IT"),
+				LocalDateTime.now()
+		));
+
+		Person p2 = new Person(
+				"Ananya",
+				"Kumari",
+				"anakum@gmail.com",
+				25
+		);
+		p2.setPersonIdCard(new PersonIdCard("12834",p2));
+		p2.addBook(new Book("The Clean Coder", LocalDateTime.now())); // With the help of the method
+		p2.addBook(new Book("The Clean Coder", LocalDateTime.now().plusDays(4)));
+		//List.of(
+		//new Book("The Clean Coder", LocalDateTime.now()),
+		//new Book("The Design Pattern", LocalDateTime.now())));
+
+		Person p3 = new Person(
+				"Akash",
+				"Kumar",
+				"akakum@gmail.com",
+				24
+		);
+		p3.setPersonIdCard(new PersonIdCard("12238",p3));
+
+
+		List<Person> people = new ArrayList<>();
+		people = List.of(p1, p2, p3);
+
+		personRepository.saveAll(people);
+
+
+
+	}
+}
---
+++ src/main/java/pd/workshop/springjpadatademo/model/Person.java
@@ -0,0 +1,112 @@
+package pd.workshop.springjpadatademo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+// Entity(O) ---> class ------- Table(R) ---> database table ----> Mapping --> Hibernate
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name ="Person")
+@Table(
+        name = "person",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "person_email_unique", columnNames = "email")
+        }
+)
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(
+            name = "id",
+            nullable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "first_name",
+            nullable = false
+    )
+    private String firstName;
+
+    @Column(
+            name = "last_name",
+            nullable = false
+    )
+    private String lastName;
+
+    @Column(
+            name = "email",
+            nullable = false
+    )
+    private String email;
+
+    @Column(
+            name = "age",
+            nullable = false
+    )
+    private Integer age;
+
+    @OneToOne(
+            mappedBy = "person",
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE}
+    )
+    private PersonIdCard personIdCard;
+
+    @OneToMany(
+            mappedBy = "person",
+            orphanRemoval = true,
+            cascade = CascadeType.PERSIST,
+            fetch = FetchType.LAZY
+    )
+    private List<Book> books = new ArrayList<>();
+
+    @OneToMany(
+            mappedBy = "person",
+            orphanRemoval = true,
+            cascade = CascadeType.PERSIST,
+            fetch = FetchType.LAZY
+    )
+    private List<Enrollment> enrollments = new ArrayList<>();
+
+
+
+    public void addBook(Book book) {
+        if(!this.books.contains(book)){
+            this.books.add(book);
+            book.setPerson(this);
+        }
+    }
+    public void removeBook(Book book){
+        if(this.books.contains(book)){
+            this.books.remove(book);
+            book.setPerson(null);
+        }
+    }
+
+    public void addEnrollment(Enrollment enrollment) {
+        if(!this.enrollments.contains(enrollment)){
+            this.enrollments.add(enrollment);
+            enrollment.setPerson(this);
+        }
+    }
+    public void removeEnrollment(Enrollment enrollment){
+        if(this.enrollments.contains(enrollment)){
+            this.enrollments.remove(enrollment);
+            enrollment.setPerson(null);
+        }
+    }
+
+    public Person(String firstName, String lastName, String email, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+}
